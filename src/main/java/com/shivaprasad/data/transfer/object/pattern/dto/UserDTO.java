17ffa72e18153538dd8537fe9fb08b296712492a
Shiva Prasad Gurram <gurramshiva05@gmail.com>
2022-11-19 19:00:26 +0530

    Added UserDTO

---
+++ src/main/java/com/shivaprasad/data/transfer/object/pattern/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.shivaprasad.data.transfer.object.pattern.dto;
+
+public class UserDTO {
+
+    private int id;
+    private String name;
+
+    public UserDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
